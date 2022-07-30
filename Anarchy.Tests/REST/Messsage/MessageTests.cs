61af22834174f3c7ce37cda54bc5dd2e039e1c70
andreas-henning <976431@outlook.com>
2022-07-30 11:57:25 +0200

    Renamed project 'Tests' to 'Anarchy.Tests'

---
+++ Anarchy.Tests/REST/Messsage/MessageTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Discord
+{
+    [TestClass]
+    public class MessageTests
+    {
+        [TestMethod]
+        public void SendMessage()
+        {
+            const string content = "The simplest possible message.";
+
+            var msg = Globals.Client.SendMessage(
+                Globals.Settings.ChannelId,
+                content);
+
+            Assert.AreEqual(content, msg.Content);
+        }
+    }
+}
