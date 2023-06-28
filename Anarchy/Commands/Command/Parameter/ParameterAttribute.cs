007635c7bd61f82d192bd859f8abce904a9feda0
xi <xi@doxbin.gay>
2023-06-28 21:44:47 +0100

    Update ParameterAttribute.cs

--- Anarchy/Commands/Command/Parameter/ParameterAttribute.cs
+++ Anarchy/Commands/Command/Parameter/ParameterAttribute.cs
@@ -1,17 +1,1 @@
-﻿using System;
 
-namespace Discord.Commands
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ParameterAttribute : Attribute
-    {
-        public string Name { get; private set; }
-        public bool Optional { get; private set; }
-
-        public ParameterAttribute(string name, bool optional = false)
-        {
-            Name = name;
-            Optional = optional;
-        }
-    }
-}
