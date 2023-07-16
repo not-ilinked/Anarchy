475b9364eb059b77a2e62fd51a9e426e7aa8caac
purpl3r0se <nightly@riseup.net>
2023-07-16 04:30:06 -0700

    See extended description.
    - Added DiscordUserProfileUser class (weird name because it's hard to fit in with everything else)
    - Added UserProfile, Nitro and BoostingSince to DiscordProfile
    - Added CertifiedModerator, Spammer (not a badge see comment in code), ActiveDeveloper badge to DiscordBadge enum
    - Added GlobalName, BannerColor, AccentColor, AvatarDecoration to DiscordProfileUser
    - Added Pronouns, Themecolors, PopoutAnimationParticleType, Emoji to DiscordUserProfileUser class
    - Added AccountSettingsUpdate with alot of stuff moved from UserProfileUpdate

---
+++ Anarchy/REST/User/Profile/DiscordUserProfileUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Discord
+{
+    public class DiscordUserProfileUser
+    {
+        [JsonProperty("pronouns")]
+        public string Pronouns { get; private set; }
+
+        [JsonProperty("theme_colors")]
+        public List<int> ThemeColors { get; private set; }
+
+        [JsonProperty("popout_animation_particle_type")] // i don't know what the type/value of this is supposed to be so i'll just set it as string for now
+        public string PopoutAnimationParticleType { get; private set; }
+
+        [JsonProperty("emoji")]
+        public string Emoji { get; private set; }
+    }
+}
--- Anarchy/REST/User/Profile/UserProfileUpdate.cs
+++ Anarchy/REST/User/Profile/UserProfileUpdate.cs
@@ -7,58 +7,6 @@ namespace Discord
     /// </summary>
     public class UserProfileUpdate
     {
-        private readonly DiscordParameter<string> NameProperty = new DiscordParameter<string>();
-        [JsonProperty("username")]
-        public string Username
-        {
-            get { return NameProperty; }
-            set { NameProperty.Value = value; }
-        }
-
-        public bool ShouldSerializeUsername()
-        {
-            return NameProperty.Set;
-        }
-
-        internal DiscordParameter<uint> DiscriminatorProperty = new DiscordParameter<uint>();
-        [JsonProperty("discriminator")]
-        public uint Discriminator
-        {
-            get { return DiscriminatorProperty; }
-            set { DiscriminatorProperty.Value = value; }
-        }
-
-        public bool ShouldSerializeDiscriminator()
-        {
-            return DiscriminatorProperty.Set;
-        }
-
-        private readonly DiscordParameter<string> EmailProperty = new DiscordParameter<string>();
-        [JsonProperty("email")]
-        public string Email
-        {
-            get { return EmailProperty; }
-            set { EmailProperty.Value = value; }
-        }
-
-        public bool ShouldSerializeEmail()
-        {
-            return EmailProperty.Set;
-        }
-
-        private readonly DiscordParameter<DiscordImage> AvatarProperty = new DiscordParameter<DiscordImage>();
-        [JsonProperty("avatar")]
-        public DiscordImage Avatar
-        {
-            get { return AvatarProperty; }
-            set { AvatarProperty.Value = value; }
-        }
-
-        public bool ShouldSerializeAvatar()
-        {
-            return AvatarProperty.Set;
-        }
-
         private readonly DiscordParameter<string> _bioParam = new DiscordParameter<string>();
         [JsonProperty("bio")]
         public string Biography
@@ -69,20 +17,14 @@ namespace Discord
 
         public bool ShouldSerializeBiography() => _bioParam.Set;
 
-        private readonly DiscordParameter<DiscordImage> _bannerParam = new DiscordParameter<DiscordImage>();
-        [JsonProperty("banner")]
-        public DiscordImage Banner
+        private readonly DiscordParameter<string> _proParam = new DiscordParameter<string>();
+        [JsonProperty("pronouns")]
+        public string Pronouns
         {
-            get { return _bannerParam; }
-            set { _bannerParam.Value = value; }
+            get { return _proParam; }
+            set { _proParam.Value = value; }
         }
 
-        public bool ShouldSerializeBanner() => _bannerParam.Set;
-
-        [JsonProperty("password")]
-        public string Password { get; set; }
-
-        [JsonProperty("new_password")]
-        public string NewPassword { get; set; }
+        public bool ShouldSerializePronouns() => _proParam.Set;
     }
 }
